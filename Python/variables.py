28a71fcf499b78e9af024966b958fb002daf4f04
rajnish-09 <rajnish9861@gmail.com>
2026-03-22 17:31:37 +0545

    feat: implement basic program using variables

---
+++ Python/variables.py
@@ -0,0 +1,10 @@
+''' 
+We don't need to specify data type of a variable while declaring them
+Rules for creating identifier:
+1. Use only english letter (A-Z, a-z)
+2. Digit (0-9)
+3. Can start with _
+'''
+name = "Rajnish stha"
+age = 22
+print("My name is: ",name, "\nI am ",age," years old")
