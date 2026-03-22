1676c40f71c84b4b107c95573c441052579e94a9
rajnish-09 <rajnish9861@gmail.com>
2026-03-22 17:38:43 +0545

    feat: learn data types

--- Python/variables.py
+++ Python/variables.py
@@ -3,8 +3,27 @@ We don't need to specify data type of a variable while declaring them
 Rules for creating identifier:
 1. Use only english letter (A-Z, a-z)
 2. Digit (0-9)
-3. Can start with _
+3. Can start with _ but not with other special characters 
 '''
 name = "Rajnish stha"
 age = 22
 print("My name is: ",name, "\nI am ",age," years old")
+
+'''
+There are different types of data types in python. They are:
+1. Integer -> +ve, -ve or 0 values
+    E.g
+        radius = 5
+2. String -> Text
+    E.g
+        name = 'Rajesh'
+3. Float -> Decimal  
+    E.g
+        pi = 3.14
+4. Boolean -> True/False
+    E.g
+        isMale = True
+'''
+
+# Print type of a variable. 
+print(type(name)) #this will return str meaning String
