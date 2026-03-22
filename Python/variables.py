a7afaf1ac67ef9bb5cee4ec42f0da4d62d40890a
rajnish-09 <rajnish9861@gmail.com>
2026-03-22 17:50:01 +0545

    feat: add style guide for naming variables

--- Python/variables.py
+++ Python/variables.py
@@ -1,29 +1,31 @@
-''' 
-We don't need to specify data type of a variable while declaring them
-Rules for creating identifier:
-1. Use only english letter (A-Z, a-z)
-2. Digit (0-9)
-3. Can start with _ but not with other special characters 
-'''
-name = "Rajnish stha"
+# ----------------------------
+# Variables in Python
+# ----------------------------
+
+name = "Rajnish Stha"
 age = 22
-print("My name is: ",name, "\nI am ",age," years old")
 
-'''
-There are different types of data types in python. They are:
-1. Integer -> +ve, -ve or 0 values
-    E.g
-        radius = 5
-2. String -> Text
-    E.g
-        name = 'Rajesh'
-3. Float -> Decimal  
-    E.g
-        pi = 3.14
-4. Boolean -> True/False
-    E.g
-        isMale = True
-'''
+print("My name is: ", name)
+print("I am ",age," years old")
 
-# Print type of a variable. 
-print(type(name)) #this will return str meaning String
+# ----------------------------
+# Data Types in Python
+# ----------------------------
+
+radius = 5          # Integer
+pi = 3.14           # Float
+is_male = True      # Boolean
+
+# Check data types
+print(type(name))    # str
+print(type(age))     # int
+print(type(pi))      # float
+print(type(is_male)) # bool
+
+
+# ----------------------------
+# Style Guid for variables
+# ----------------------------
+my_name = 'Rajnish'     # snake_case -> we mainly use this type of naming variable
+myName = 'Rajnish'      #camelCase
+MyName = 'Rajnish'      #PascalCase
