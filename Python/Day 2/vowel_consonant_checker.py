6d53d253c9296944e30e3e4effb96a016c5ac509
rajnish-09 <rajnish9861@gmail.com>
2026-03-23 15:18:52 +0545

    feat: implement vowel and consonant checker

---
+++ Python/Day 2/vowel_consonant_checker.py
@@ -0,0 +1,6 @@
+value = input("Enter a character: ")
+value = value.lower()
+if(value == 'a' or value == 'e' or value == 'i' or value == 'o' or value == 'u'):
+    print('Vowel')
+else:
+    print('Consonant')
