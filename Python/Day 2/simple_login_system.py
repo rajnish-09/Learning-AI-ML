e2914f834b0efc2888901ee1868bd72bd850ebfd
rajnish-09 <rajnish9861@gmail.com>
2026-03-23 15:21:48 +0545

    feat: implement simple login system

---
+++ Python/Day 2/simple_login_system.py
@@ -0,0 +1,7 @@
+# Simple login checker to login if username = rajnish and password = 1234
+username = input("Enter username: ")
+password = input("Enter password: ")
+if(username == 'rajnish' and password == '1234'):
+    print("Login success")
+else:
+    print("Login Failed")
