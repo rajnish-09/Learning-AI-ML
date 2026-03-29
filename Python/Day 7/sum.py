8b1af18ebaf323a4b4b3f6c4f9d15d9b79ddd122
rajnish-09 <rajnish9861@gmail.com>
2026-03-29 17:20:12 +0545

    feat: implement sum using function

---
+++ Python/Day 7/sum.py
@@ -0,0 +1,7 @@
+# Program to print sum of two numbers using function
+def sum(a,b):
+    return a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f"Sum of {num1} and {num2}: {sum(num1,num2)}")
