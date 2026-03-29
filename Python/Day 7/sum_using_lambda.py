ad648daa410fd3cae4b50a70b9d8f69e26e98415
rajnish-09 <rajnish9861@gmail.com>
2026-03-29 20:31:33 +0545

    feat: implement sum using lambda function

---
+++ Python/Day 7/sum_using_lambda.py
@@ -0,0 +1,7 @@
+# Program to find sum of two number using lambda function
+
+sum = lambda a,b : a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f'Sum: {sum(num1, num2)}')
