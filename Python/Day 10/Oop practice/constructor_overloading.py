a3729143f8200dc8502887c51af9c9a70089dced
rajnish-09 <rajnish9861@gmail.com>
2026-04-05 08:47:23 +0545

    refactor: change constructor

--- Python/Day 10/Oop practice/constructor_overloading.py
+++ Python/Day 10/Oop practice/constructor_overloading.py
@@ -1,29 +1,23 @@
-# Create class Person that allows the constructor to work with name only, name + age, name + age + address
-
 class Person:
-    def __init__(self, name = None, age = None, address = None):
-        if name is not None and age is not None and address is not None:
+    def __init__(self, name=None, age=None, address=None):
+        self.name = name
+        self.age = age
+        self.address = address
+
+        if name and age and address:
             print("Constructor with 3 parameters")
-            self.name = name
-            self.age = age
-            self.address = address
-
-        elif name is not None and age is not None:
+        elif name and age:
             print("Constructor with 2 parameters")
-            self.name = name
-            self.age = age
-
+        elif name:
+            print("Constructor with 1 parameter")
         else:
-            print("Constructor with a parameters")
-            self.name = name
+            print("Default constructor")
 
 
 p1 = Person("Ram")
 p2 = Person("Hari", 22)
 p3 = Person("Shyam", 25, "Ktm")
 
-print(p1.name)
-print(p2.name, p2.age)
+print(p1.name, p1.age, p1.address)
+print(p2.name, p2.age, p2.address)
 print(p3.name, p3.age, p3.address)
-
-            
